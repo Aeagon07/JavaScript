c260f30d351d97f9c9566fa4cf12c838a6f84c39
Rushikesh Godase <godaserushikesh07@gmail.com>
2025-06-18 02:09:40 +0000

    Call Stack

---
+++ 07_Function/callStack.js
@@ -0,0 +1,14 @@
+// Important for the Interview
+
+// Javascript is single threaded langague... which means sabhi ko ik process bana ke treat kiya jata hai 
+
+// Memomery Creation Phase and Execution Phase these are two phase created after code..
+
+// MCP => jo bhi variables hai unne jagah allocate hoti hai meomery ke andar .. Jo execute nahi hote iss ke anadr 
+// EP => Iss ke andar hi aapke + - aur jo execution code ka wo hota hai..
+
+// Jitni baar js ke andar funtion execute hote hai utni baar unke andar new execution contest create hota hai..
+// Uske andar new variable environment and execution thread hoti hai
+
+// Call Stack => Basically it store the information of the function call 
+// it is nothing but the stack which follow the LIFO principle..
