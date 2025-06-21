34eac5158dd64a67873258b315892294b13d92f7
Rushikesh Godase <godaserushikesh07@gmail.com>
2025-06-21 17:10:57 +0000

    Terniary Operator And NCO  Done

---
+++ 09_Control_Flow/terniary.js
@@ -0,0 +1,13 @@
+// Terniary Operator
+
+// Condition ? true : false
+
+const iceTea = 60
+iceTea >= 80 ? console.log("True") : console.log("False") // => False
+
+// It is basically checks the both codition and which condition satisfied the condition it will executed like if it is true then 1st one or false then next one 
+
+const icePrice = 100;
+icePrice >= 80 ? console.log("True") : console.log("False") // => True
+
+// Bas dhyaan rakhna synatx ka ki kab true ? ke baad hai aur false condition : ke baad...
--- 09_Control_Flow/truthy.js
+++ 09_Control_Flow/truthy.js
@@ -1,50 +1,47 @@
-// Ham kai baar maan lete hai ki ye true hai ya phir ye false hai..
-const userEmail = ""
+// // Ham kai baar maan lete hai ki ye true hai ya phir ye false hai..
+// const userEmail = ""
 
-if(userEmail){
-    console.log("Got User Email");
-}else{
-    console.log("Don't have user email")
-}
+// if(userEmail){
+//     console.log("Got User Email");
+// }else{
+//     console.log("Don't have user email")
+// }
 
-/* "" empty string hai to wo false consider kar rahe hai aur string ke andar kuch likh hai "Rushi" usse ham true consider kar rahe hai..
- */
+// /* "" empty string hai to wo false consider kar rahe hai aur string ke andar kuch likh hai "Rushi" usse ham true consider kar rahe hai..
+//  */
 
-// Falsy Values
-false, 0, -0, BigInt (0n), "", null, undefined, NaN
+// // Falsy Values
+// false, 0, -0, BigInt (0n), "", null, undefined, NaN
 
-// Rest all are the truthy value
-"0", 'false', " ", [], {}, function(){} 
-// above all are very important for iterview perspective
+// // Rest all are the truthy value
+// "0", 'false', " ", [], {}, function(){} 
+// // above all are very important for iterview perspective
 
-// How to check they are empty or not
-if(userEmail.length === 0){
-    console.log("Array is Empty")
-}
+// // How to check they are empty or not
+// if(userEmail.length === 0){
+//     console.log("Array is Empty")
+// }
 
-// Object
-const myObj = {}
+// // Object
+// const myObj = {}
 
-if(Object.keys(myObj).length === 0){
-    console.log("Object is Empty.")
-}
+// if(Object.keys(myObj).length === 0){
+//     console.log("Object is Empty.")
+// }
 
-// Object.keys(myObj) => gives you array
-// then .length gives you the size of that array...
+// // Object.keys(myObj) => gives you array
+// // then .length gives you the size of that array...
 
-// Nullish Coalescing Operator (??) : null undefined
+// // Nullish Coalescing Operator (??) : null undefined
 
-let val1;
-val1 = 5 ?? 10; 
-val1 = null ?? 10; // ye us hota hai agar kisi complex function se value aa rahi hai to wo assign karlo nahi to safty ke liye null ko consider karlo..
+// let val1;
+// val1 = 5 ?? 10; 
+// val1 = null ?? 10; // ye us hota hai agar kisi complex function se value aa rahi hai to wo assign karlo nahi to safty ke liye null ko consider karlo..
 
-val1 = undefined ?? 15;
+// val1 = undefined ?? 15;
 
-val1 = null ?? 10 ?? 20;
+// val1 = null ?? 10 ?? 20;
 
-console.log(val1);
+// // console.log(val1);
 
-// NCO => It is just fall back operator used when you have to use the dat from the application.. 
-
-// Terniary Operator
-
+// // NCO => It is just fall back operator used when you have to use the dat from the application.. 
