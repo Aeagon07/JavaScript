696ab13dcd3c285dd0debdb215b70909d2da2ca4
Rushikesh Godase <godaserushikesh07@gmail.com>
2025-08-14 11:41:53 +0000

    calls

--- 16_ADV/call.js
+++ 16_ADV/call.js
@@ -1,9 +1,14 @@
+// In Interview => asked for what is call -> call transfer the current execution contest to another function => kind of there is function inside the function..
 function setUser(username){
     // complex DB calls
     this.username = username;
+    console.log("called!");
 }
 function createUser(username, email, pass){
-    setUser(username)
+    // setUser(username)
+    setUser.call(this, username) // but just writting .call() never resolve the problem it just call but never change occur in the object 
+    // when you use call() then optinally we can write the this at the start
+
     // this.username = username; ye mat karo sidha call the setUser
     this.email = email;
     this.pass = pass;
@@ -16,3 +21,8 @@ console.log(chai);
 // did they actualy call the fucntion or not?
 // -> Simply, this method is not called,
 // -> Technically JS just gives refercence not given the call
+
+// If you see the it calls but not needed call cause there is problem which arieses is it remove callme() and his execustion contest so the varible decalare inside the call is not refer although
+
+// so you have to hold reference no leaving them or removing their EC
+// -> we use .call()
